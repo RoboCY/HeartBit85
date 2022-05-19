9ad45d85b8a3a5979a808b030c535a39b90a5920
michalis_d <m.demetriou13@gmail.com>
2022-05-19 20:22:17 +0300

    refactoring

--- HeartBit85.ino
+++ HeartBit85.ino
@@ -1,66 +1,16 @@
 
-//#include <Arduino.h>
-
 #include <Adafruit_NeoPixel.h>
+#include "RoboAnim.h"
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define PIN PB0
-#define NUMPIXELS 24
-#define DELAYVAL 15
+#define LED_PIN PB0
 #define BTN_PIN PB1
-#define MAX_ANIM_LOOPS 3
+#define NUMPIXELS 24
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-//int animationLoops;
-int r,g,b;
-int animationLoops = 0;
-int middlePixel=NUMPIXELS/2;
-
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 int currentAnim = 0;
-
-void animation(int x);
-
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos, float brightness) {
-  if(WheelPos < 85) {
-   return pixels.Color((WheelPos * 3)*brightness, (255 - WheelPos * 3)*brightness, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return pixels.Color((255 - WheelPos * 3)*brightness, 0, (WheelPos * 3)*brightness);
-  } else {
-   WheelPos -= 170;
-   return pixels.Color(0, (WheelPos * 3)*brightness, (255 - WheelPos * 3)*brightness);
-  }
-}
-
-
-void setColorAndBrightness(int pixel, int r, int g, int b, float brightness) {
-  pixels.setPixelColor(pixel, pixels.Color(r*brightness, g*brightness, b*brightness));
-}
-
-
-void pixelTurnOff(int pixel) {
-  pixels.setPixelColor(pixel, pixels.Color(0,0,0));
-}
-
-
-void stripClear() {
-  for(int i=0; i<NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0,0,0));
-  }
-  // setColorAndBrightness(currentAnim, 255,0,0, 0.1); // TEMP
-  pixels.show();  
-}
-
-
-void playNextAnimation() {
-    playAnimation(currentAnim);
-    stripClear();
-}
 
 
 void setup() {
@@ -69,322 +19,50 @@ void setup() {
   #endif
   pinMode(BTN_PIN,INPUT_PULLUP);
   pixels.begin();
-  stripClear();
+  RoboAnim::init(&pixels);
+  RoboAnim::stripClear();
 }
 
 
 void loop() {
   if(digitalRead(BTN_PIN)==LOW){
-    playNextAnimation();
-    delay(200);
+    playAnimation(currentAnim);
+    delay(100);
   }
-
   delay(10);
 }
 
 
-
 void playAnimation(int x){
-  uint16_t i, j;
-  animationLoops = 0;
-  r = 255;
-  g = random(0,100);
-  b = random(0,150);  
-
   switch(x){
-
     case 0:
-    
-      while(animationLoops<MAX_ANIM_LOOPS){
-        for(int i=0; i<NUMPIXELS; i++) {
-          pixels.setPixelColor(i, pixels.Color(r, g, b));
-          pixels.show();
-          delay(DELAYVAL);
-        }
-        for(int i=NUMPIXELS; i>-1; i--) { // For each pixel...
-          pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-          pixels.show();
-          delay(DELAYVAL);
-        }  
-        delay(1500);
-        
-        animationLoops++;
-      }
+      RoboAnim::anim0();
       break;
-      
     case 1:
-    
-      while(animationLoops<MAX_ANIM_LOOPS){
-        
-        for(i=0; i<=NUMPIXELS/2; i++) {
-          setColorAndBrightness(middlePixel+i, r, g, b, 1);
-          setColorAndBrightness(middlePixel-i, r, g, b, 1);
-          pixels.show();
-          delay(DELAYVAL*2);
-        }
-        
-        for(i=0; i<=NUMPIXELS/2; i++) {
-          pixelTurnOff(middlePixel+i);
-          pixelTurnOff(middlePixel-i);
-          pixels.show();
-          delay(DELAYVAL);
-        }
-
-        delay(1500);
-        
-        animationLoops++;
-      }
-    
+      RoboAnim::anim1();
       break;
-
-
     case 2:
-    
-      while(animationLoops<MAX_ANIM_LOOPS){
-
-        
-        for(i=0; i<=NUMPIXELS/2; i++) {
-          // animate right half (up->down)
-          pixelTurnOff(middlePixel+i-3);
-          setColorAndBrightness(middlePixel+i-2, r, g, b, 0.2);
-          setColorAndBrightness(middlePixel+i-1, r, g, b, 0.5);
-          setColorAndBrightness(middlePixel+i, r, g, b, 1);
-          // animate left half (up->down)
-          pixelTurnOff(middlePixel-i+3);
-          setColorAndBrightness(middlePixel-i+2, r, g, b, 0.2);
-          setColorAndBrightness(middlePixel-i+1, r, g, b, 0.5);
-          setColorAndBrightness(middlePixel-i, r, g, b, 1);
-          pixels.show();
-          delay(DELAYVAL*2);
-        }
-        
-        for(i=0; i<=NUMPIXELS/2; i++) {
-          // animate right half (down->up)
-          pixelTurnOff(i-3);
-          setColorAndBrightness(i-2, r, g, b, 0.2);
-          setColorAndBrightness(i-1, r, g, b, 0.5);
-          setColorAndBrightness(i, r, g, b, 1);
-          // animate left half (down->up)
-          pixelTurnOff(NUMPIXELS-i+3);
-          setColorAndBrightness(NUMPIXELS-i+2, r, g, b, 0.2);
-          setColorAndBrightness(NUMPIXELS-i+1, r, g, b, 0.5);
-          setColorAndBrightness(NUMPIXELS-i, r, g, b, 1);
-          pixels.show();
-          delay(DELAYVAL*2);
-        }
-
-        stripClear();
-        delay(1000);
-        animationLoops++;
-      }
-    
+      RoboAnim::anim2();
       break;
-
     case 3:
-
-
-      while(animationLoops<MAX_ANIM_LOOPS){
-     
-
-        for(i=0; i<NUMPIXELS; i++) {
-          pixels.setPixelColor(i, Wheel((i*13) & 255, 0.8));
-          pixels.show();
-          delay(DELAYVAL);
-        }
-
-        delay(100);
-
-        for(i=0; i<NUMPIXELS; i++) {
-          pixelTurnOff(i);
-          pixels.show();
-          delay(DELAYVAL);
-        }
-        delay(1000);
-               
-        animationLoops++;
-      }
-      
-               
+      RoboAnim::anim3();  
       break;
-
-
     case 4:
-
-
-      j = 1;
-      while(animationLoops<MAX_ANIM_LOOPS*8){
-        
-       
-
-        for(i=0; i<NUMPIXELS; i++) {
-          if(i%2 == 0) {
-            pixels.setPixelColor(i, Wheel((i*j)+30 & 255, 0.8));
-          }
-          else {
-            pixels.setPixelColor(i, Wheel(((i-1)*j)+30 & 255, 0.3));
-          }
-        }
-        pixels.show();
-
-        delay(100);
-
-        for(i=0; i<NUMPIXELS; i++) {
-          if(i%2 == 0) {
-            pixels.setPixelColor(i, Wheel(((i-1)*j)+30 & 255, 0.2));
-          }
-          else {
-            pixels.setPixelColor(i, Wheel((i*j)+30 & 255, 0.5));
-          }
-        }
-        pixels.show();
-
-        delay(100);
-               
-        animationLoops++;
-        j++;
-      }
-
-      
-      
-               
-      break;    
-      
-              
+      RoboAnim::anim4();
+      break;
     case 5:
-      
-      // heart beat
-
-      while(animationLoops<MAX_ANIM_LOOPS){
-        
-        // fade in
-        for(j=1; j<NUMPIXELS; j++) {
-          for(i=0; i<NUMPIXELS; i++) {
-            setColorAndBrightness(i, r, g, b, ((float)j/(float)NUMPIXELS));
-          }
-          pixels.show();
-          delay(2);
-        }
-
-        // fade out
-        for(j=NUMPIXELS; j>0; j--) {
-          for(i=0; i<NUMPIXELS; i++) {
-            setColorAndBrightness(i, r, g, b, ((float)j/(float)NUMPIXELS));
-          }
-          pixels.show();
-          delay(2);
-        }
-
-        // fade in
-        for(j=1; j<NUMPIXELS; j++) {
-          for(i=0; i<NUMPIXELS; i++) {
-            setColorAndBrightness(i, r, g, b, ((float)j/(float)NUMPIXELS));
-          }
-          pixels.show();
-          delay(2);
-        }
-
-        // fade out
-        for(j=NUMPIXELS; j>0; j--) {
-          for(i=0; i<NUMPIXELS; i++) {
-            setColorAndBrightness(i, r, g, b, ((float)j/(float)NUMPIXELS));
-          }
-          pixels.show();
-          delay(15);
-        }        
-
-        stripClear();
-        delay(500);
-
-        animationLoops++;
-      }
+      RoboAnim::anim5();
       break;
-    
-
-
     case 6:
-
-
-      // for(int x=0; x<NUMPIXELS; x++) {
-      //   setColorAndBrightness(x, 255,0,0, 1);
-      //   // pixels.setPixelColor(x, pixels.Color(r, g, b));
-      //   pixels.show();
-      //   delay(DELAYVAL);
-      // }
-
-      for(int j=0; j<=NUMPIXELS/2; j++) {
-
-        for(int i=0; i<=NUMPIXELS/2; i++) {
-          // animate left half (up->down)
-          setColorAndBrightness(middlePixel-i, r, g, b, 1);
-          if((NUMPIXELS/2)-i > j) pixelTurnOff(middlePixel-i+1);
-          pixels.show();
-          delay(DELAYVAL);
-        }
-        // delay(DELAYVAL);
-
-        for(int i=0; i<=NUMPIXELS/2; i++) {
-          // animate right half (up->down)
-          setColorAndBrightness(middlePixel+i, r, g, b, 1);
-          if(i < (NUMPIXELS/2)-j) pixelTurnOff(middlePixel+i-1);
-          pixels.show();
-          delay(DELAYVAL);
-        }
-        // delay(DELAYVAL);
-
-      }
-
-      delay(1000);
-    
+      RoboAnim::anim6();
       break;
-
     case 7:
-
-      int p=0;
-      int currentPixel;
-
-      
-      for(i=0; i<(NUMPIXELS*3)+1; i++) {
-        currentPixel = i%NUMPIXELS;
-
-        p = currentPixel-5 >= 0 ? currentPixel-5 : (currentPixel-5)+NUMPIXELS;
-        pixelTurnOff(p);
-
-        p = currentPixel-4 >= 0 ? currentPixel-4 : (currentPixel-4)+NUMPIXELS;
-        setColorAndBrightness(p, r, g, b, 0.1);
-
-        p = currentPixel-3 >= 0 ? currentPixel-3 : (currentPixel-3)+NUMPIXELS;
-        setColorAndBrightness(p, r, g, b, 0.2);
-
-        p = currentPixel-2 >= 0 ? currentPixel-2 : (currentPixel-2)+NUMPIXELS;
-        setColorAndBrightness(p, r, g, b, 0.4);
-
-        p = currentPixel-1 >= 0 ? currentPixel-1 : (currentPixel-1)+NUMPIXELS;
-        setColorAndBrightness(p, r, g, b, 0.6);
-
-        setColorAndBrightness(currentPixel, r, g, b, 1);
-
-        pixels.show();
-        delay(40);
-      }
-      
-
-
-    
+      RoboAnim::anim7();
       break;
-
-
-
-
-
-    case 8:
-
-
-      currentAnim = 0;
-      playAnimation(0);
+    default:
+      currentAnim = -1;
       break;    
   }
-
+  RoboAnim::stripClear();
   currentAnim++;
-
-
 }
---
+++ RoboAnim.h
@@ -0,0 +1,300 @@
+#include <Adafruit_NeoPixel.h>
+
+namespace RoboAnim {
+
+    Adafruit_NeoPixel *pixels; // reference to the pixels object
+    int numPixels = 0;
+    int middlePixel = 0;
+    int r,g,b;
+
+    void init(Adafruit_NeoPixel *p) {
+        pixels = p;
+        numPixels = pixels->numPixels();
+        middlePixel = numPixels/2;
+    }
+
+
+    /************************/
+    /*** HELPER FUNCTIONS ***/
+    /************************/
+
+    void pixelPaint(int pixel, float brightness) {
+        pixels->setPixelColor(pixel, pixels->Color(r*brightness, g*brightness, b*brightness));
+    }
+
+
+    void pixelTurnOff(int pixel) {
+        pixels->setPixelColor(pixel, pixels->Color(0,0,0));
+    }
+
+
+    void stripClear() {
+        for(int i=0; i<numPixels; i++) {
+            pixels->setPixelColor(i, pixels->Color(0,0,0));
+        }
+        pixels->show();  
+    }
+
+
+    uint32_t Wheel(byte WheelPos, float brightness) {
+        if(WheelPos < 85) {
+            return pixels->Color((WheelPos * 3)*brightness, (255 - WheelPos * 3)*brightness, 0);
+        } 
+        else if(WheelPos < 170) {
+            WheelPos -= 85;
+            return pixels->Color((255 - WheelPos * 3)*brightness, 0, (WheelPos * 3)*brightness);
+        }
+        else {
+            WheelPos -= 170;
+            return pixels->Color(0, (WheelPos * 3)*brightness, (255 - WheelPos * 3)*brightness);
+        }
+    }
+
+    void generateRandomColor() {
+        r = 255;
+        g = random(0,100);
+        b = random(0,150);         
+    }
+
+
+
+
+    /***********************/
+    /* ANIMATION FUNCTIONS */
+    /***********************/
+
+
+    void anim0() {
+        generateRandomColor();
+        int animationLoops = 0;
+        while(animationLoops<3){
+            for(int i=0; i<numPixels; i++) {
+            pixels->setPixelColor(i, pixels->Color(r, g, b));
+            pixels->show();
+            delay(15);
+            }
+            for(int i=numPixels; i>-1; i--) { // For each pixel...
+            pixels->setPixelColor(i, pixels->Color(0, 0, 0));
+            pixels->show();
+            delay(15);
+            }  
+            delay(1000);
+            animationLoops++;
+        }        
+    }
+
+
+
+
+    void anim1() {
+        generateRandomColor();
+        int animationLoops = 0;
+        while(animationLoops<3){
+            for(int i=0; i<=middlePixel; i++) {
+                pixelPaint(middlePixel+i, 1);
+                pixelPaint(middlePixel-i, 1);
+                pixels->show();
+                delay(30);
+            }
+            for(int i=0; i<=middlePixel; i++) {
+                pixelTurnOff(middlePixel+i);
+                pixelTurnOff(middlePixel-i);
+                pixels->show();
+                delay(30);
+            }
+            delay(1000);
+            animationLoops++;
+        }    
+    }
+
+
+
+
+    void anim2() {
+        generateRandomColor();
+        int animationLoops = 0;
+        while(animationLoops<3){
+            for(int i=0; i<=numPixels/2; i++) {
+                // animate right half (up->down)
+                pixelTurnOff(middlePixel+i-3);
+                pixelPaint(middlePixel+i-2, 0.2);
+                pixelPaint(middlePixel+i-1, 0.5);
+                pixelPaint(middlePixel+i, 1);
+                // animate left half (up->down)
+                pixelTurnOff(middlePixel-i+3);
+                pixelPaint(middlePixel-i+2, 0.2);
+                pixelPaint(middlePixel-i+1, 0.5);
+                pixelPaint(middlePixel-i, 1);
+                pixels->show();
+                delay(30);
+            }     
+            for(int i=0; i<=numPixels/2; i++) {
+                // animate right half (down->up)
+                pixelTurnOff(i-3);
+                pixelPaint(i-2, 0.2);
+                pixelPaint(i-1, 0.5);
+                pixelPaint(i, 1);
+                // animate left half (down->up)
+                pixelTurnOff(numPixels-i+3);
+                pixelPaint(numPixels-i+2, 0.2);
+                pixelPaint(numPixels-i+1, 0.5);
+                pixelPaint(numPixels-i, 1);
+                pixels->show();
+                delay(30);
+            }
+            stripClear();
+            delay(1000);
+            animationLoops++;
+        }
+    }  
+
+
+
+
+    void anim3() {
+        int animationLoops = 0;
+        while(animationLoops<3){
+            for(int i=0; i<numPixels; i++) {
+                pixels->setPixelColor(i, Wheel((i*13) & 255, 0.8));
+                pixels->show();
+                delay(15);
+            }
+            delay(300);
+            for(int i=0; i<numPixels; i++) {
+                pixelTurnOff(i);
+                pixels->show();
+                delay(15);
+            }
+            delay(1000);
+            animationLoops++;
+        }
+    }
+
+
+
+
+    void anim4() {
+        int j = 1;
+        int animationLoops = 0;
+        while(animationLoops<18){
+            for(int i=0; i<numPixels; i++) {
+                if(i%2 == 0) {
+                    pixels->setPixelColor(i, Wheel((i*j)+30 & 255, 0.8));
+                }
+                else {
+                    pixels->setPixelColor(i, Wheel(((i-1)*j)+30 & 255, 0.3));
+                }
+            }
+            pixels->show();
+            delay(100);
+            for(int i=0; i<numPixels; i++) {
+                if(i%2 == 0) {
+                    pixels->setPixelColor(i, Wheel(((i-1)*j)+30 & 255, 0.2));
+                }
+                else {
+                    pixels->setPixelColor(i, Wheel((i*j)+30 & 255, 0.5));
+                }
+            }
+            pixels->show();
+            delay(100);
+            animationLoops++;
+            j++;
+        }
+    }
+
+
+
+
+    void anim5() {
+        generateRandomColor();
+        int animationLoops = 0;
+        while(animationLoops<3){
+            // fade in
+            for(int j=1; j<numPixels; j++) {
+                for(int i=0; i<numPixels; i++) {
+                    pixelPaint(i, ((float)j/(float)numPixels));
+                }
+                pixels->show();
+                delay(2);
+            }
+            // fade out
+            for(int j=numPixels; j>0; j--) {
+                for(int i=0; i<numPixels; i++) {
+                    pixelPaint(i, ((float)j/(float)numPixels));
+                }
+                pixels->show();
+                delay(2);
+            }
+            // fade in
+            for(int j=1; j<numPixels; j++) {
+                for(int i=0; i<numPixels; i++) {
+                    pixelPaint(i, ((float)j/(float)numPixels));
+                }
+                pixels->show();
+                delay(2);
+            }
+            // fade out
+            for(int j=numPixels; j>0; j--) {
+                for(int i=0; i<numPixels; i++) {
+                    pixelPaint(i, ((float)j/(float)numPixels));
+                }
+                pixels->show();
+                delay(10);
+            }
+            stripClear();
+            delay(500);
+            animationLoops++;
+        }
+    }
+
+
+
+
+    void anim6() {
+        generateRandomColor();
+        for(int j=0; j<=numPixels/2; j++) {
+            for(int i=0; i<=numPixels/2; i++) {
+                // animate left half (up->down)
+                pixelPaint(middlePixel-i, 1);
+                if((numPixels/2)-i > j) pixelTurnOff(middlePixel-i+1);
+                pixels->show();
+                delay(10);
+            }
+            for(int i=0; i<=numPixels/2; i++) {
+                // animate right half (up->down)
+                pixelPaint(middlePixel+i, 1);
+                if(i < (numPixels/2)-j) pixelTurnOff(middlePixel+i-1);
+                pixels->show();
+                delay(10);
+            }
+        }
+        delay(500);
+    }
+
+
+
+
+    void anim7() {
+        generateRandomColor();
+        int p=0;
+        int currentPixel;
+        for(int i=0; i<(numPixels*3)+1; i++) {
+            currentPixel = i%numPixels;
+            p = currentPixel-5 >= 0 ? currentPixel-5 : (currentPixel-5)+numPixels;
+            pixelTurnOff(p);
+            p = currentPixel-4 >= 0 ? currentPixel-4 : (currentPixel-4)+numPixels;
+            pixelPaint(p, 0.1);
+            p = currentPixel-3 >= 0 ? currentPixel-3 : (currentPixel-3)+numPixels;
+            pixelPaint(p, 0.2);
+            p = currentPixel-2 >= 0 ? currentPixel-2 : (currentPixel-2)+numPixels;
+            pixelPaint(p, 0.4);
+            p = currentPixel-1 >= 0 ? currentPixel-1 : (currentPixel-1)+numPixels;
+            pixelPaint(p, 0.6);
+            pixelPaint(currentPixel, 1);
+            pixels->show();
+            delay(40);
+        }
+    }
+
+
+}
